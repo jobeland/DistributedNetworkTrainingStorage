8c31d428842047503bcde23493d6331359ea5d8a
jobeland <jordan.beland@gmail.com>
2015-11-24 23:52:48 -0800

    update proxy to block on calls

--- StorageAPI/StorageAPI/ParseProxy.cs
+++ StorageAPI/StorageAPI/ParseProxy.cs
@@ -27,15 +27,18 @@ namespace StorageAPI
             var networkParseFormat = new ParseObject(_networkVersion);
             networkParseFormat["jsonNetwork"] = JsonConvert.SerializeObject(network.GetGenes());
             networkParseFormat["eval"] = eval;
-            await networkParseFormat.SaveAsync();
+            networkParseFormat.SaveAsync().Wait();
         }
 
         public async Task<ITrainingSession> GetBestSession()
         {
-            var result = await ParseCloud.CallFunctionAsync<ParseObject>("bestNetwork", new Dictionary<string, object> { { "networkVersion", _networkVersion } });
+            var task = ParseCloud.CallFunctionAsync<ParseObject>("bestNetwork", new Dictionary<string, object> { { "networkVersion", _networkVersion } });
+            task.Wait();
+            var result = task.Result;
             var networkGenes = JsonConvert.DeserializeObject<NeuralNetworkGene>((string)result["jsonNetwork"]);
             var network = NeuralNetworkFactory.GetInstance().Create(networkGenes);
-            return new FakeTrainingSession(network, (double)result["eval"]);
+            var session = new FakeTrainingSession(network, (double)result["eval"]);
+            return session;
         }
     }
 }
