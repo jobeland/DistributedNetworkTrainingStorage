2c8a29c87eff971edae4ab6606302f4880c6d061
jobeland <jordan.beland@gmail.com>
2016-08-22 23:48:01 -0700

    add node js proxy

---
+++ StorageAPI/StorageAPI/Proxies/NodeJS/NodeJSProxy.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ArtificialNeuralNetwork;
+using ArtificialNeuralNetwork.Factories;
+using ArtificialNeuralNetwork.Genes;
+using NeuralNetwork.GeneticAlgorithm;
+using Newtonsoft.Json;
+
+namespace StorageAPI.Proxies.NodeJS
+{
+    public class NodeJSProxy : IStorageProxy
+    {
+        private readonly int _networkVersion;
+        private readonly string _apiKey;
+        private readonly string _baseUrl;
+
+        public NodeJSProxy(int networkVersion, string baseUrl, string apiKey)
+        {
+            _networkVersion = networkVersion;
+            _baseUrl = baseUrl;
+            _apiKey = apiKey;
+        }
+
+        public ITrainingSession GetBestSession()
+        {
+            using (var client = new HttpClient())
+            {
+                // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiKey);
+                var networkEndpoint = _baseUrl + "/network/" + _networkVersion;
+                var response = client.GetAsync(networkEndpoint).Result;
+                var responseObj = response.Content.ReadAsAsync<NodeJsMessage>().Result;
+                var networkGenes = JsonConvert.DeserializeObject<NeuralNetworkGene>(responseObj.NetworkGenes);
+                var network = NeuralNetworkFactory.GetInstance().Create(networkGenes);
+                var session = new FakeTrainingSession(network, responseObj.Eval);
+                return session;
+            }
+        }
+
+        public async Task<ITrainingSession> GetBestSessionAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiKey);
+                var networkEndpoint = _baseUrl + "/network/" + _networkVersion;
+                var response = await client.GetAsync(networkEndpoint);
+                var responseObj = await response.Content.ReadAsAsync<NodeJsMessage>();
+                var networkGenes = JsonConvert.DeserializeObject<NeuralNetworkGene>(responseObj.NetworkGenes);
+                var network = NeuralNetworkFactory.GetInstance().Create(networkGenes);
+                var session = new FakeTrainingSession(network, responseObj.Eval);
+                return session;
+            }
+        }
+
+        public void StoreNetwork(INeuralNetwork network, double eval)
+        {
+            using (var client = new HttpClient())
+            {
+                // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiKey);
+                var networkEndpoint = _baseUrl + "/network";
+                var message = new NodeJsMessage
+                {
+                    Eval = eval,
+                    Version = _networkVersion,
+                    NetworkGenes = network.GetGenes().ToString()
+                };
+                client.PostAsJsonAsync(networkEndpoint, message).Wait();
+            }
+        }
+
+        public async Task StoreNetworkAsync(INeuralNetwork network, double eval)
+        {
+            using (var client = new HttpClient())
+            {
+                // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiKey);
+                var networkEndpoint = _baseUrl + "/network";
+                var message = new NodeJsMessage
+                {
+                    Eval = eval,
+                    Version = _networkVersion,
+                    NetworkGenes = network.GetGenes().ToString()
+                };
+                await client.PostAsJsonAsync(networkEndpoint, message);
+            }
+        }
+    }
+}
---
+++ StorageAPI/StorageAPI/Proxies/NodeJS/NodeJsMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageAPI.Proxies.NodeJS
+{
+    public class NodeJsMessage
+    {
+        public string NetworkGenes { get; set; }
+        public double Eval { get; set; }
+        public int Version { get; set; }
+    }
+}
